83707021aab7efd96c38dcb99a1bcf0684a08287
Tommy Sanchez <tommy-s05@hotmail.com>
2025-05-15 21:46:38 -0400

    Add medication-related enums for schedule and type.

---
+++ resources/js/Enums/MedicationScheduleFrequencyEnum.ts
@@ -0,0 +1,6 @@
+export enum MedicationScheduleFrequencyEnum {
+    DAILY = 'daily',
+    SPECIFIC_DAYS = 'specific_days',
+    INTERVAL_IN_DAYS = 'interval_in_days',
+    HOURLY_INTERVAL = 'hourly_interval',
+}
---
+++ resources/js/Enums/MedicationTypeEnum.ts
@@ -0,0 +1,11 @@
+export enum MedicationTypeEnum {
+    PILL = 'pill',
+    CAPSULE = 'capsule',
+    SYRUP = 'syrup',
+    INJECTION = 'injection',
+    INHALER = 'inhaler',
+    DROPS = 'drops',
+    OINTMENT = 'ointment',
+    PATCH = 'patch',
+    OTHER = 'other',
+}
