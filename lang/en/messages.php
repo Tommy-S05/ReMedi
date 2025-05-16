036fbca239daa8bcb723e7cecda847b03499b2f1
Tommy Sanchez <tommy-s05@hotmail.com>
2025-05-15 21:29:58 -0400

    Add English and Spanish localization files with the laravel-lang package

---
+++ lang/en/messages.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'medication_added_successfully' => 'Medication added successfully!',
+    'failed_to_add_medication' => 'Failed to add medication. Please try again.',
+    'medication_deleted_successfully' => 'Medication deleted successfully.',
+    'failed_to_delete_medication' => 'Failed to delete medication.',
+    'medication_updated_successfully' => 'Medication updated successfully!',
+    'failed_to_update_medication' => 'Failed to update medication.',
+];
---
+++ lang/es/messages.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'medication_added_successfully' => '¡Medicamento añadido exitosamente!',
+    'failed_to_add_medication' => 'Error al añadir el medicamento. Por favor, inténtelo de nuevo.',
+    'medication_deleted_successfully' => 'Medicamento eliminado exitosamente.',
+    'failed_to_delete_medication' => 'Error al eliminar el medicamento.',
+    'medication_updated_successfully' => '¡Medicamento actualizado exitosamente!',
+    'failed_to_update_medication' => 'Error al actualizar el medicamento.',
+];
