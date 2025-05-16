bd981b8c0cb1fb54a3b37ff3397d15e678b606be
Tommy Sanchez <tommy-s05@hotmail.com>
2025-05-15 21:48:16 -0400

    Add routes for managing medications with CRUD functionality

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,27 @@
 <?php
 
+use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
+Route::get('/', function() {
     return Inertia::render('Welcome');
 })->name('home');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function() {
+    Route::get('dashboard', function() {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+    // Rutas para Medicamentos
+    Route::get('medications', [Controllers\MedicationController::class, 'index'])->name('medications.index');
+    Route::get('medications/create', [Controllers\MedicationController::class, 'create'])->name('medications.create');
+    Route::post('medications', [Controllers\MedicationController::class, 'store'])->name('medications.store');
+    // Route::get('medications/{medication}', [Controllers\MedicationController::class, 'show'])->name('medications.show');
+    Route::get('medications/{medication}/edit', [Controllers\MedicationController::class, 'edit'])->name('medications.edit');
+    Route::put('medications/{medication}', [Controllers\MedicationController::class, 'update'])->name('medications.update');
+    Route::delete('medications/{medication}', [Controllers\MedicationController::class, 'destroy'])->name('medications.destroy');
+});
+
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
